ced459e21f088ea2ce68b92e66cb7fd922d1f0d4
Brian Triplett <brian.triplett@ncino.com>
2018-09-18 15:18:00 -0400

    Updated to include querying test data

--- circle-stats.py
+++ circle-stats.py
@@ -21,13 +21,16 @@ class AnalyzeBuilds(object):
     GITHUB_ORG = os.environ.get('GITHUB_ORG')
     CI_TOKEN = os.environ.get('CI_TOKEN')
     CI_API_URL_PREFIX = "https://circleci.com/api/v1.1/project/github"
-    get_recent_builds_URL = CI_API_URL_PREFIX + "/{github_org}/{repo}?circle-token={ci_token}&limit=100&offset={offset_counter}&filter={filter}"
+    get_recent_builds_URL = CI_API_URL_PREFIX + "/{github_org}/{repo}/tree/release?circle-token={ci_token}&limit=100&offset={offset_counter}&filter={filter}"
     get_build_details_URL = CI_API_URL_PREFIX + "/{github_org}/{repo}/{build_num}?circle-token={ci_token}"
+    get_test_data_URL = CI_API_URL_PREFIX + "/{github_org}/{repo}/{build_num}/tests?circle-token={ci_token}"
 
     def __init__(self):
         self.test_times_by_class = {}
         self.median_time_by_class = {}
         self.REPO = sys.argv[1]
+        self.processed_builds = []
+        self.test_results = []
 
         if len(sys.argv) > 2:
             self.build_iterations = int(sys.argv[2])/100
@@ -45,9 +48,10 @@ class AnalyzeBuilds(object):
         :return: Dict recent_builds
         """
         recent_builds = []
-        build_stats = []
 
         for offset_counter in range(0, self.build_iterations):
+
+            print "Getting build " + offset_counter*100 + " of " + self.build_iterations
 
             recents_url = AnalyzeBuilds.get_recent_builds_URL.format(
                 github_org=self.GITHUB_ORG,
@@ -59,13 +63,14 @@ class AnalyzeBuilds(object):
 
             recent_builds = self._make_json_request(recents_url)
  
-            self._process_build_data(recent_builds, build_stats)
+            self._process_build_data(recent_builds)
 
-        self.write_csv_file("build.csv", build_stats)
+        self.write_csv_file("build.csv", self.processed_builds, ["job_name","start_time", "status", "build_time", "failure_step"])
+        self.write_csv_file("tests.csv", self.test_results,["build", "test_class", "test_name","full_name", "result", "run_time", "message"])
 
         return recent_builds
 
-    def _process_build_data(self, builds, processed_builds):
+    def _process_build_data(self, builds):
 
         for build in builds:
             if "workflows" in build:
@@ -83,13 +88,15 @@ class AnalyzeBuilds(object):
             eastern = timezone('US/Eastern')
             build_time_string =  build_datetime_utc.astimezone(eastern).strftime('%Y-%m-%d %H:%M:%S')
 
-            processed_builds.append(
-                {"start_time": build_time_string, 
+            self._get_test_results(build)
+
+            self.processed_builds.append({
+                "start_time": build_time_string, 
                 "status": build["status"], 
                 "build_time": build["build_time_millis"], 
                 "job_name":job_name, 
-                "failure_step": failure_step}
-            )        
+                "failure_step": failure_step
+            })        
 
     def _get_failure_reason(self, build):
 
@@ -107,6 +114,30 @@ class AnalyzeBuilds(object):
                 if action["status"] != "success":
                     return step["name"]
 
+    def _get_test_results(self, build):
+
+        test_url = AnalyzeBuilds.get_test_data_URL.format(
+            github_org=self.GITHUB_ORG,
+            repo=self.REPO,
+            ci_token=self.CI_TOKEN,
+            build_num=build["build_num"]
+        )
+
+        test_data = self._make_json_request(test_url)
+
+        for test in test_data["tests"]:
+            # Temporary check for failures only
+            if test["result"] == "failure":
+                self.test_results.append({
+                    "build": build["build_num"],
+                    "test_class": test["classname"],
+                    "test_name": test["name"],
+                    "full_name": test["classname"] + "." + test["name"],
+                    "result": test["result"],
+                    "run_time": test["run_time"],
+                    "message": test["message"]
+                })
+
     def _make_json_request(self, url):
         logging.info("Making request at URL: %s" % url)
         r = requests.get(url=url)
@@ -119,13 +150,13 @@ class AnalyzeBuilds(object):
         return json.loads(r.text)
 
     @staticmethod
-    def write_csv_file(csv_output_filename, csv_data):
+    def write_csv_file(csv_output_filename, csv_data, field_names):
 
         with open(csv_output_filename, 'w') as csvfile:
             csv_writer = csv.DictWriter(
                 csvfile,
-                fieldnames=["job_name","start_time", "status", "build_time", "failure_step"],
-                delimiter=",", quotechar='"', escapechar='\\', quoting=csv.QUOTE_NONE)
+                fieldnames=field_names,
+                delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL)
             csv_writer.writeheader()
             for row in csv_data:
                 csv_writer.writerow(row)
